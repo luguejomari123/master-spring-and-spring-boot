3154f316d20c0344aa949012413343b3ce11e0e6
Jomari Lugue <jclugue@sviisca.com>
2024-05-16 17:47:52 +0800

    finsihed up to step 98

--- 11-webapp/myfirstwebapp/src/main/java/com/jom/springboot/myfirstwebapp/hello/SayHelloController.java
+++ 11-webapp/myfirstwebapp/src/main/java/com/jom/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -30,8 +30,13 @@ public class SayHelloController {
 		return sb.toString();
 	}
 	
+	
+	//"say-hello-jsp" => sayHello.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/todos.jsp
 	@RequestMapping("say-hello-jsp")
-	@ResponseBody
 	public String sayHelloJsp() {
 		return "sayHello";
 	}
---
+++ 11-webapp/myfirstwebapp/src/main/java/com/jom/springboot/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,24 @@
+package com.jom.springboot.myfirstwebapp.login;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	//  http://localhost:8080/login?name=jom
+	@RequestMapping("login")
+	public String gotoLogin(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
+		logger.debug("Request param is {}", name);
+		logger.info("I want this printed at info level {}", name);
+		System.out.println("Request param is " + name); //NOT RECOMMENDED FOR PROD CODE
+		
+		return "login";
+	}
+}
