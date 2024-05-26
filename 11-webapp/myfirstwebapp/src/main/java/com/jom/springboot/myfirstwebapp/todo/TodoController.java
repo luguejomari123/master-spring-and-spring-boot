b4557008764c7d7b7256a9a7e59bb054523c0ae8
Jomari Lugue <jclugue@sviisca.com>
2024-05-26 15:11:05 +0800

    done up to step 123

--- 11-webapp/myfirstwebapp/src/main/java/com/jom/springboot/myfirstwebapp/todo/TodoController.java
+++ 11-webapp/myfirstwebapp/src/main/java/com/jom/springboot/myfirstwebapp/todo/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.validation.Valid;
@@ -45,7 +46,32 @@ public class TodoController {
 		}
 		
 		String username = (String)model.get("name");
-		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
+		todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
+		return "redirect:list-todos";
+	}
+	
+	@RequestMapping("delete-todo")
+	public String deleteTodo(@RequestParam int id) {
+		todoService.deleteById(id);
+		return "redirect:list-todos";
+	}
+	
+	@RequestMapping(value="update-todo", method= RequestMethod.GET)
+	public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+		Todo todo = todoService.findById(id);
+		model.put("todo", todo);
+		return "todo";
+	}
+	
+	@RequestMapping(value="update-todo", method= RequestMethod.POST)
+	public String updateTodo(ModelMap model,@Valid Todo todo, BindingResult result) {
+		if(result.hasErrors()) {
+			return "todo";
+		}
+		
+		String username = (String)model.get("name");
+		todo.setUsername(username);
+		todoService.updateTodo(todo);
 		return "redirect:list-todos";
 	}
 	
--- 11-webapp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ 11-webapp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -1,11 +1,11 @@
-<html>
-	<head>
-		<title>Welcome dog</title>
-	</head>
-	<body>
-		<div class="container">
-			<h1>Welcome sup ${name}</h1>
-			<div><a href="list-todos">Manage</a> your todos</div>
-		</div>
-	</body>
-</html>
+<%@ include file="common/header.jspf"%>
+<%@ include file="common/navigation.jspf"%>
+
+<div class="container">
+	<h1>Welcome sup ${name}</h1>
+	<div>
+		<a href="list-todos">Manage</a> your todos
+	</div>
+</div>
+
+<%@ include file="common/footer.jspf"%>
